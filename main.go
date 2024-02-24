d398b4654e72e6e23fb343ccc6e3b525f3ec69b6
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-24 13:58:34 -0500

    server created

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
+	fmt.Println("Listening on port :6379")
+
+	// Create a new server
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
@@ -19,4 +25,21 @@ func main() {
 	}
 
 	defer conn.Close() // close connection once finished
+
+	for {
+		buf := make([]byte, 1024)
+
+		// read message from client
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		// ignore request and send back a PONG
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
